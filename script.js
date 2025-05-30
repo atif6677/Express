95cad4a1094d3466164efc8c3764c8af11b0cbc1
atif6677 <mohd.atif2531@gmail.com>
2025-05-30 11:06:38 +0530

    postman

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-
 const express = require('express');
 const app = express();
+const port = 3000;
 
+// Middleware to parse JSON bodies (for POST requests)
+app.use(express.json());
 
-function attachUser(req, res, next) {
-    req.user = "Guest"; 
-    next(); 
-}
-
-
-app.get('/welcome', attachUser, (req, res) => {
-    
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+// GET /orders route
+app.get('/orders', (req, res) => {
+  res.send('Here is the list of all orders.');
 });
 
-const port = 3000;
+// POST /orders route
+app.post('/orders', (req, res) => {
+  res.send('A new order has been created.');
+});
+
+// GET /users route
+app.get('/users', (req, res) => {
+  res.send('Here is the list of all users.');
+});
+
+// POST /users route
+app.post('/users', (req, res) => {
+  res.send('A new user has been added.');
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    console.log(`Open your browser to http://localhost:${port}/welcome`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
