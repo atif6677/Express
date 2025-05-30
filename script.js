a2dd7490b1ce39d61ec38f7d61cf59662eea16cb
atif6677 <mohd.atif2531@gmail.com>
2025-05-30 20:43:56 +0530

    Unknown route

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-// Middleware to parse JSON bodies (for POST requests)
 app.use(express.json());
 
-// GET /orders route
-app.get('/orders', (req, res) => {
-  res.send('Here is the list of all orders.');
+app.get('/products', (req, res) => {
+  res.send("Here is the list of all products.");
 });
 
-// POST /orders route
-app.post('/orders', (req, res) => {
-  res.send('A new order has been created.');
+app.post('/products', (req, res) => {
+  res.send("A new product has been added.");
 });
 
-// GET /users route
-app.get('/users', (req, res) => {
-  res.send('Here is the list of all users.');
+app.get('/categories', (req, res) => {
+  res.send("Here is the list of all categories.");
 });
 
-// POST /users route
-app.post('/users', (req, res) => {
-  res.send('A new user has been added.');
+app.post('/categories', (req, res) => {
+  res.send("A new category has been created.");
 });
 
-// Start the server
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
